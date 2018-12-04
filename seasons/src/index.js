be4e228463e025145c7d64b57273b1bc44366a79
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2018-12-04 10:16:50 -0500

    add season name

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import SeasonDisplay from "./SeasonDisplay";
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // this is the only time do direct assign state
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -33,7 +28,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading ...</div>;
