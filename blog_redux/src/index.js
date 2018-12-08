c01e334d2ea684886ecf633abe5d808b8973e640
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2018-12-08 13:34:48 -0500

    blog redux app

---
+++ blog_redux/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import combineReducers from "./reducers";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
