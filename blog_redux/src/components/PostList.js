7db604cb8e597d8136fe44f2036ef8c2e25cbe0c
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2018-12-08 13:49:12 -0500

    fix store

--- blog_redux/src/components/PostList.js
+++ blog_redux/src/components/PostList.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-
+import { connect } from "react-redux";
+import { fetchPost } from "../actions";
 export class PostList extends Component {
+  componentDidMount() {
+    return this.props.fetchPost;
+  }
+
   render() {
     return <div>PostList</div>;
   }
 }
 
-export default PostList;
+export default connect(
+  null,
+  { fetchPost }
+)(PostList);
