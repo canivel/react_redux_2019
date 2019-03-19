3e7bfaabc0fa31024bf350196b0e028fd263ea64
Danilo Canivel <d.canivel@gmail.com>
2019-03-19 13:39:04 -0400

    basic hooks app

---
+++ hooks-basic/src/components/ResourceList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import useResource from "./useResources";
+
+const ResourceList = ({ resource }) => {
+  const resources = useResource(resource);
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ResourceList;
