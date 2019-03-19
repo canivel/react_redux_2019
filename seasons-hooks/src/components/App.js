fca5c88c7428b56eee7ed5e3585adc8cb7956686
Danilo Canivel <d.canivel@gmail.com>
2019-03-19 15:37:26 -0400

    season hooks refactor

---
+++ seasons-hooks/src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import useLocation from "./useLocation";
+
+const App = () => {
+  const [lat, errorMessage] = useLocation();
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />;
+  }
+
+  return <div className="border red">{content}</div>;
+};
+
+export default App;
---
+++ seasons-hooks/src/components/useLocation.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export default () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []); //empty array makes it run only once on mount
+
+  return [lat, errorMessage];
+};
